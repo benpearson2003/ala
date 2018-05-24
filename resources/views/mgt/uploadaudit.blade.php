91b6570433f69e7e3f3efd7f8a3eb2ce5f343857
Benjamin Pearson <bpearson@arklegaudit.gov>
2018-05-24 08:56:11 -0500

    listing filenames, but unable to remove files and unable to add files one at a time

--- resources/views/mgt/uploadaudit.blade.php
+++ resources/views/mgt/uploadaudit.blade.php
@@ -8,14 +8,34 @@
 @section('content')
     <form action={{ route('mgt/uploadAudit')}} method="POST" enctype="multipart/form-data">
         {{ csrf_field() }}
-        Book title:
-        <br />
-        <input type="text" name="title" />
-        <br /><br />
-        Logo:
-        <br />
-        <input type="file" name="audit" />
-        <br /><br />
+        <input type="file" id="files" name="audits" value="Upload Audits" multiple/>
+        <div id="selectedFiles"></div>
         <input type="submit" value=" Save " />
     </form>
+    <script>
+        var selDiv = "";
+
+        document.addEventListener("DOMContentLoaded", init, false);
+
+        function init() {
+            document.querySelector('#files').addEventListener('change', handleFileSelect, false);
+            selDiv = document.querySelector("#selectedFiles");
+        }
+
+        function handleFileSelect(e) {
+
+            if(!e.target.files) return;
+
+            selDiv.innerHTML = "";
+
+            var files = e.target.files;
+            for(var i=0; i<files.length; i++) {
+                var f = files[i];
+
+                selDiv.innerHTML += f.name + "<br/>";
+
+            }
+
+        }
+    </script>
 @endsection
